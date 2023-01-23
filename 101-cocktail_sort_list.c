a2283e6928e36bf03be2a3e91b346b585a12ee1a
OmarKhalil10 <omar.khalil498@gmail.com>
2023-01-23 14:33:42 -0800

    fix 101-cocktail_sort_list.c

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -33,7 +33,7 @@ void cocktail_sort_list(listint_t **list)
 	listint_t *node;
 	int swap_done = 1;
 
-	if (list == '\0' || (*list) == '\0' || (*list)->next == '\0')
+	if (list == 0 || (*list) == 0 || (*list)->next == 0)
 		return;
 	node = *list;
 	while (swap_done == 1)
